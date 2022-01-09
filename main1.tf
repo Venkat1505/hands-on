e1a283bc1f17ada2c19e2c7cdec95865ed0e7e9f
Venkateswarlu Ganjikunta <Venkat.ganjikunta@gmail.com>
2022-01-09 15:53:36 -0600

    changes

--- main1.tf
+++ main1.tf
@@ -1,6 +1,6 @@
 module "s3" {
     source = "./modules/s3"
     s3_count = 2
-    bucket_name = ["hello1456626","helloi27276"]
+    bucket_name = ["hello14566261","helloi272762"]
     acl = "private"
 }
